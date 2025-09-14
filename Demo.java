10912ebf508af561e573e973bb1aff60acc86308
MohammadHusainShaikh <mohammad.husain2760@gmail.com>
2025-09-14 12:42:25 +0530

    [ADDED]:Mohammd Husain - In this code we will see Package in java

---
+++ Demo.java
@@ -0,0 +1,10 @@
+
+import other.tools.AdvCalc;
+
+public class Demo {
+    public static void main(String a[]){
+        AdvCalc ob = new AdvCalc();
+        System.out.println(ob.add(5, 03));
+        System.out.println(ob.div(4, 6));
+    }
+}
